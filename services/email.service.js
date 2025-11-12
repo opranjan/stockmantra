1033d2dc57312cb57fc74b33fca52ca54cef1fdf
opranjan <bordoloipritish97@gmail.com>
2025-11-12 13:20:04 +0530

    updated

--- services/email.service.js
+++ services/email.service.js
@@ -5,7 +5,7 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: parseInt(process.env.SMTP_PORT || "465"),
-  secure: process.env.SMTP_SECURE === "true", // true for port 465
+  secure: process.env.SMTP_SECURE === "false", // true for port 465
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
